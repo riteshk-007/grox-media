00844e140010af3723ef5ea7badb6b77f8351224
riteshk-007 <codeshorts007@gmail.com>
2026-09-05 17:12:16 +0530

    feat: initialize root layout with custom fonts, SEO metadata, and analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,10 +131,29 @@ export default function RootLayout({
             gtag('config', 'G-79WH26B6DY');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MWN39MTJ');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${sharpLight.variable} ${sharpHeading.variable} font-jakarta antialiased pb-24 md:pb-16 overflow-x-hidden`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWN39MTJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <PwaRegister />
         <AppShell>{children}</AppShell>
       </body>
