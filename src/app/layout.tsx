111eaa36259af9dba06d1eb62a87c1d3b6643b15
riteshk-007 <codeshorts007@gmail.com>
2026-09-06 20:04:44 +0530

    feat: initialize root layout with custom fonts, SEO metadata, and Google Tag Manager integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,7 +137,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MWN39MTJ');`}
+})(window,document,'script','dataLayer','GTM-N9RXMV37');`}
         </Script>
         {/* End Google Tag Manager */}
       </head>
@@ -147,7 +147,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MWN39MTJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9RXMV37"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
